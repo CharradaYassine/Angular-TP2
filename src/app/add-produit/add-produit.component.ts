2c29cfaafcbe43e63c09a40f73888bc9b89168a1
Charrada Yassine <charradayassine@gmail.com>
2020-11-11 17:17:43 +0100

    tp02

--- src/app/add-produit/add-produit.component.ts
+++ src/app/add-produit/add-produit.component.ts
@@ -1,14 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Produit } from '../model/produit.model';
+import { ProduitService } from '../services/produit.service';
 @Component({
   selector: 'app-add-produit',
   templateUrl: './add-produit.component.html',
 })
 export class AddProduitComponent implements OnInit {
-
-  constructor() { }
+  
+  newProduit = new Produit();
+  
+  msg :  string;
+  constructor(private produitService : ProduitService) { }
 
   ngOnInit(): void {
+  }
+  ajouterProduit(){
+  //console.log(this.newProduit);  
+  this.produitService.ajouterProduit(this.newProduit);
+  this.msg ="Produit " + this.newProduit.nomProduit + " ajouter avec succés ";
+
   }
 
 }
